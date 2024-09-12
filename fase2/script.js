b934344c94e4ec69d6dc3d9fba0ee7e60175ec74
Mrocanw01f <miguelcanedoads@gmail.com>
2024-09-12 16:55:51 -0300

    1.3

---
+++ fase2/script.js
@@ -0,0 +1,10 @@
+const cenario = document.getElementById("cenario"):
+const lucario = document.getElementById("lucario");
+
+const larguraCenario = cenario.offsetWidth;
+const alturaCenario = cenario.offsetHeight;
+
+const larguraLucario = lucario.offsetWidth;
+const auturaLucario = lucario.offsetHeight;
+
+//https://www.youtube.com/watch?v=MYbJzIDjEno
